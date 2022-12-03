7739c9d9e22d7c929328ff918146aee5333ce8ee
cballevre <celestin.ballevre@protonmail.com>
2022-12-03 21:00:14 +0100

    Card design

--- lib/pages/recognition_sheet_list.dart
+++ lib/pages/recognition_sheet_list.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sami/utils/palette.dart';
-import 'package:sami/pages/create_sample.dart';
+import 'package:sami/pages/recognition_sheet.dart';
+
 
 class RecognitionSheetListPage extends StatefulWidget {
   const RecognitionSheetListPage({super.key});
@@ -17,14 +18,30 @@ class _RecognitionSheetListPageState extends State<RecognitionSheetListPage> {
     return StreamBuilder(
       stream: FirebaseFirestore.instance.collection("recognition_sheets").snapshots(),
       builder: (context, snapshot) {
-        return !snapshot.hasData ? Text('PLease Wait') : ListView.builder(
-          itemCount: snapshot.data?.docs.length,
-          itemBuilder: (context, index) {
-            Map<String, dynamic>? recognitionSheets = snapshot.data?.docs.elementAt(index).data();
-            return Text(recognitionSheets?['sampler_lastname']);
-          },
+        return !snapshot.hasData ? Text('PLease Wait') : Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
+          child: ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic>? recognitionSheets = snapshot.data?.docs.elementAt(index).data();
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                elevation: 5,
+                child: InkWell(child: ListTile(
+                  leading: Icon(Icons.icecream),
+                  title: Text("List item ${recognitionSheets?['sampler_lastname']}"),
+                  subtitle: Text('Icream is good for health'),
+                ), onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const RecognitionSheetPage(),
+                    ),
+                  );
+                }),
+              );
+            }));
+        },
         );
       }
-    );
-  }
+
 }
